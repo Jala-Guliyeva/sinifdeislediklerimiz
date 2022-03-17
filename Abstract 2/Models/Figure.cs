fb9ab0c28b53eac33f552a6fa975f81ba8806c1c
Jala Guliyeva <tu7knjupn@aztu.local>
2022-03-17 02:32:26 -0700

    file added

---
+++ Abstract 2/Models/Figure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_2.Models
+{
+    abstract class Figure
+    {
+        public abstract int CalcArea(int area);
+    }
+    
+    
+}
---
+++ Abstract 2/Models/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_2.Models
+{
+    internal class Square
+    {
+        public int 
+        public int side
+        {
+            get
+            {
+                
+                
+
+            }
+            set
+            {
+            }
+        }
+    }
+}
